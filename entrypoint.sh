697cc537570850a60caf9efd3884cb871e59b638
Robin Appelman <robin@icewind.nl>
2020-07-21 20:34:53 +0200

    fix cmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-xmllint --format "$1" --output __formatted.xml
+xmllint --format "$1" --output /__formatted.xml
 
-cmp --silent "$!" __formatted.xml
+cmp --silent "$1" /__formatted.xml
 NEEDED_FORMATTING=$?
 
-mv __formatted.xml "$1"
+mv /__formatted.xml "$1"
 
 exit $NEEDED_FORMATTING
